ccad3174b6d2d1825555ab95ebf23e9d25436484
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2023-11-23 19:31:58 -0300

    feat: add user reducer

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,23 +1,35 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 
 // Components
 import Cart from "../cart/index";
 
 // Styles
 import * as Styles from "./styles";
+import { loginUser, logoutUser } from '../../redux/user/actions';
 
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
+  const { currentUser } = useSelector(rootReducer => rootReducer.userReducer);
+  const dispatch = useDispatch();
 
   const handleCartClick = () => {
     setCartIsVisible(true);
+  };
+
+  const handleLogInLogOutClick = () => {
+    if (currentUser) {
+      dispatch(logoutUser());
+    } else {
+      dispatch(loginUser({ name: 'Vinicius', age: 23 }));
+    }
   };
 
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        <div>Login</div>
+        <div onClick={handleLogInLogOutClick}>{ currentUser ? 'Sair' : 'Login' }</div>
         <div onClick={handleCartClick}>Carrinho</div>
       </Styles.Buttons>
 
---
+++ src/redux/user/reducer.js
@@ -0,0 +1,18 @@
+import * as UserActionTypes from './action-types';
+
+const initialState = {
+  currentUser: null,
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case UserActionTypes.LOGIN:
+      return { ...state, currentUser: action.payload };
+    case UserActionTypes.LOGOUT:
+      return { ...state, currentUser: null };
+    default:
+      return state;
+  };
+};
+
+export default userReducer;
